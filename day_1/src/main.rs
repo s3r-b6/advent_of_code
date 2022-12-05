1a42e5c77888947559a175ba73cac16e4c0ba1f7
Sergio <sergio.bermejo123@gmail.com>
2022-12-05 14:04:13 +0100

    solved part 2 of day1

--- day_1/src/main.rs
+++ day_1/src/main.rs
@@ -3,6 +3,9 @@ use std::fs::File;
 use std::io::prelude::*;
 use std::io::BufReader;
 
+//Part1: Find the Elf carrying the most Calories. How many total Calories is that Elf carrying?
+//TODO Part2: Find the top three Elves carrying the most Calories. How many Calories are those Elves carrying in total?
+
 fn main() {
     let file =
         File::open(r"D:\Rust\advent_of_code\day_1\src\inventory.txt").expect("file not found!");
@@ -25,15 +28,36 @@ fn main() {
     }
 
     let mut max_value: (String, i32) = ("".to_string(), 0);
+    let mut sec_max_value: (String, i32) = ("".to_string(), 0);
+    let mut third_max_value: (String, i32) = ("".to_string(), 0);
     if !elf_list.is_empty() {
         for elf in elf_list {
-            if max_value.1 < elf.1 {
-                max_value = (elf.0, elf.1)
-            };
+            if third_max_value.1 < elf.1 {
+                if sec_max_value.1 < elf.1 {
+                    if max_value.1 < elf.1 {
+                        third_max_value = sec_max_value;
+                        sec_max_value = max_value;
+                        max_value = elf;
+                    } else {
+                        third_max_value = sec_max_value;
+                        sec_max_value = elf;
+                    }
+                } else {
+                    third_max_value = elf;
+                }
+            }
         }
         println!(
             "\n\nMax_value is: {}, carried by {}\n\n",
             max_value.1, max_value.0
+        );
+        println!(
+            "\n\nSecond_max_value is: {}, carried by {}\n\n",
+            sec_max_value.1, sec_max_value.0
+        );
+        println!(
+            "\n\nThird_max_value is: {}, carried by {}\n\n",
+            third_max_value.1, third_max_value.0
         );
     } else {
         println!("\n\nerror:  no values were stored\n\n");
